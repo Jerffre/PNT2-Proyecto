b416dbd47c8e8f389146d84b00ea5ee6aaeafd19
SofiaGalvagni <sbgalvagni@gmail.com>
2024-06-25 19:02:25 -0300

    Modificaciones

--- clase-9/App.js
+++ clase-9/App.js
@@ -1,28 +1,19 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import RegisterLoginScreen from './screens/RegisterLoginScreen';
 import { HomeScreen } from './screens/HomeScreen';
-import { ProductDetailScreen } from './screens/ProductDetailScreen';
 import { useContext } from 'react';
 import { AuthContext, AuthProvider } from './context/AuthContext';
 import { MovieProvider } from './context/MovieContext';
 import { MovieDetailScreen } from './screens/MovieDetailScreen';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Navbar } from './components/Navbar';
 import { MoviesFavoritesScreen } from './screens/MoviesFavoritesScreen';
 import AboutUsScreen from './screens/AboutUsScreen';
 import HelpScreen from './screens/HelpScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NotificationsScreen } from './screens/NotificationsScreen';
-import { NotificationsProvider } from './context/NotificationsContext';
 import { FavoritesProvider } from './context/FavoritesContext';
-
-import * as Notifications from 'expo-notifications';
-import Constants from 'expo-constants';
-import { NotificationsContext, NotificationsProvider} from './context/NotificationsContext';
+import { NotificationsProvider} from './context/NotificationsContext';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
